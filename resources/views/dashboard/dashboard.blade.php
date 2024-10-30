1c32c127db81d59ba5f9c8c64afd0dd38eaf4f20
ahmdirvn <ahmadirvan9c17@gmail.com>
2024-10-30 12:41:26 +0700

    reactor view

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -21,7 +21,6 @@
     @endif
 
     <div class="row">
-
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
